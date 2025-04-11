34fcb7cbee3ba41ec8b074cde69bc68bba98c6a0
VaishEd31 <vaishnaviaghav1206@gmail.com>
2025-04-11 13:39:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function updateDateTime() {
     const now = new Date();
     const formatted = now.toLocaleString();
     document.getElementById("datetime").textContent = formatted;
-  }
+  };
   setInterval(updateDateTime, 1000);
   updateDateTime();
   
